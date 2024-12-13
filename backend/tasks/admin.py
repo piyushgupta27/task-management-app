ebd9966a6da1ccf43031bc12e35f9695e2b81af8
Piyush Gupta <piyush@jumpingminds.ai>
2024-12-13 10:40:47 +0530

    Add admin page for Task model

--- backend/tasks/admin.py
+++ backend/tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "completed", "created_at", "updated_at")
+    search_fields = ("title", "description")
+    ordering = ("-created_at",)
