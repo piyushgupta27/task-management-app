e28621e1a8369ba78d930ba68a96a18daf970611
Piyush Gupta <piyush@jumpingminds.ai>
2024-12-13 10:51:35 +0530

    Update fields in Task model

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,12 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Task(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="created_tasks"
+    )
+    updated_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="updated_tasks"
+    )
 
     def __str__(self):
         return self.title
